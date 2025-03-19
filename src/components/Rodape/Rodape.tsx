dbd7bd06f3d47bf5f570595ee39cd1ead72d1fda
Joui <jouibomfim@gmail.com>
2025-03-19 16:32:57 -0300

    tela de login da biblioteca

---
+++ src/components/Rodape/Rodape.tsx
@@ -0,0 +1,12 @@
+import estilo from './Rodape.module.css';
+
+function Rodape() {
+    return (
+        <footer className={estilo.rodape}>
+            <p>Desenvolvido por: Joui Bomfim Mendonça</p>
+            <p>Copyright</p>
+        </footer>
+    )
+}
+
+export default Rodape;
---
+++ src/components/Welcome/Welcome.tsx
@@ -0,0 +1,10 @@
+import estilo from './Welcome.module.css';
+
+function Welcome() {
+    <main className={estilo.principal}>
+        <p>Seja bem-vindo(a) à biblioteca.</p>
+        <p>Para ter uma melhor experiência, faça login no sistema.</p>
+    </main>
+}
+
+export default Welcome;
