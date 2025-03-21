7d345d50b812443397057a5b5e6fe01c100e4cf8
Joui <jouibomfim@gmail.com>
2025-03-21 09:57:48 -0300

    tela de login com css

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,15 @@ import './App.css';
 import Cabecalho from './components/Cabecalho/Cabecalho';
 import Rodape from './components/Rodape/Rodape';
 import Welcome from './components/Welcome/Welcome';
+import LoginForm from './components/LoginForm/LoginForm';
 
 function App() {
 
   return (
     <>
-      <Cabecalho />
-      <Welcome />
-      <Rodape />
+      <Cabecalho/>
+      <LoginForm />
+      <Rodape/>
     </>
   )
 }
--- src/components/Welcome/Welcome.tsx
+++ src/components/Welcome/Welcome.tsx
@@ -1,10 +1,12 @@
 import estilo from './Welcome.module.css';
 
 function Welcome() {
-    <main className={estilo.principal}>
-        <p>Seja bem-vindo(a) à biblioteca.</p>
-        <p>Para ter uma melhor experiência, faça login no sistema.</p>
-    </main>
+    return (
+        <main className={estilo.principal}>
+            <p>Seja bem-vindo(a) à biblioteca.</p>
+            <p>Para ter uma melhor experiência, faça login no sistema.</p>
+        </main>
+    );
 }
 
 export default Welcome;
