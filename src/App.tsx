3a5dc943984026f4939744465d9d764203f48313
Joui <jouibomfim@gmail.com>
2025-04-04 09:11:27 -0300

    Link funcinal para a página de Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
 import './App.css';
-import Cabecalho from './components/Cabecalho/Cabecalho';
-import Rodape from './components/Rodape/Rodape';
-import Welcome from './components/Welcome/Welcome';
-import LoginForm from './components/LoginForm/LoginForm';
-
+import AppRoutes from './routes';
 function App() {
 
   return (
     <>
-      <Cabecalho/>
-      <LoginForm />
-      <Rodape/>
+      <AppRoutes />
     </>
   )
 }
